c0ecc0e3cd538afba3248dbbc4510e807508dc7b
dapena <dapena@elespectador.com>
2024-11-25 19:58:09 +1100

    feat/technical-test: Answers and text area to improve interaction

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,19 @@ import "./styles/JSONVisualizer.css";
 import "./styles/CustomNode.css";
 import "./styles/Tooltip.css";
 import JSONVisualizer from "./components/JSONVisualizer/JSONVisualizer";
-const BASE_URL = import.meta.env.VITE_API_URL;
+import { useState } from "react";
 
 function App() {
+  const [jsonData, setJsonData] = useState();
   return (
     <>
-      <JSONVisualizer />
+      <textarea
+      placeholder="Please, enter your JSON data here..." className="Visualizer-TextArea"
+        onChange={(e) => {
+          setJsonData(e.target?.value);
+        }}
+      />
+      <JSONVisualizer className='Visualizer-Canvas' source={jsonData} />
     </>
   );
 }
--- client/src/components/JSONVisualizer/JSONVisualizer.tsx
+++ client/src/components/JSONVisualizer/JSONVisualizer.tsx
@@ -9,91 +9,29 @@ import {
   Controls,
 } from "@xyflow/react";
 import CustomNode from "../CustomNode/CustomNode";
-
+const BASE_URL = import.meta.env.VITE_API_URL;
 import "@xyflow/react/dist/style.css";
 
-const JSONToSend = {
-  user: {
-    id: 12345,
-    name: "John Doe",
-    contact: {
-      email: "johndoe@example.com",
-      phone: "+1234567890",
-    },
-    address: {
-      street: "123 Main St",
-      city: "New York",
-      zipcode: "10001",
-      geo: {
-        lat: 40.7128,
-        lng: -74.006,
-      },
-    },
-    preferences: {
-      notifications: true,
-      language: "English",
-      theme: "dark",
-    },
-  },
-  orders: [
-    {
-      order_id: 98765,
-      product: {
-        id: 5678,
-        name: "Laptop",
-        price: 999.99,
-      },
-      shipping: {
-        method: "standard",
-        cost: 5.99,
-        address: {
-          street: "456 Elm St",
-          city: "Brooklyn",
-          zipcode: "11201",
-        },
-      },
-      status: "shipped",
-      date: "2024-11-20",
-    },
-    {
-      order_id: 98766,
-      product: {
-        id: 5679,
-        name: "Headphones",
-        price: 199.99,
-      },
-      shipping: {
-        method: "express",
-        cost: 15.99,
-        address: {
-          street: "789 Oak St",
-          city: "Queens",
-          zipcode: "11354",
-        },
-      },
-      status: "processing",
-      date: "2024-11-22",
-    },
-  ],
-};
-
-const JSONVisualizer = () => {
+const JSONVisualizer = ({ source }) => {
   const [data, setData] = useState({});
   async function getData(url: string): Promise<void> {
     try {
-      const myHeaders = new Headers();
-      myHeaders.append("Content-Type", "application/json");
-      const response = await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(JSONToSend),
-        headers: myHeaders,
-      });
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
+      if (source) {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        const response = await fetch(url, {
+          method: "POST",
+          body: source,
+          headers: myHeaders,
+        });
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+        console.log(response);
 
-      const json = await response.json();
-      setData(json);
+        const json = await response.json();
+        setData(json);
+      }
     } catch (error) {
       console.error(error);
     }
@@ -111,9 +49,9 @@ const JSONVisualizer = () => {
   );
 
   useEffect(() => {
-    const url = `http://localhost:5000/api/visualize`;
+    const url = `${BASE_URL}/api/visualize`;
     getData(url);
-  }, []);
+  }, [source]);
 
   useEffect(() => {
     const _nodes = data?.newNodes || [];
